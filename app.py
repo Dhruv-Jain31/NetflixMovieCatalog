5e69ed335c81b78ca80e8b936edc3c77cb211d86
Dhruv-Jain31 <126942955+Dhruv-Jain31@users.noreply.github.com>
2024-09-18 16:34:54 +0530

    added some changes to app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ with open('data/data_movies.json', 'r') as f:
 
 @app.route("/", methods=['GET'])
 def home():
-    return "Hi!!!!!!. I am dhruv jain. This app is an API, there is no UI ;-)"
+    return "Hi!!!!!!. I am dhruv jain and this is my 12th change. This app is an API, there is no UI ;-)"
 
 
 @app.route('/discover')
